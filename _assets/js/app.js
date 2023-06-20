45548416c811ab5cba869bc0a936790cf47d49db
Andrew Burnes <andrew.burnes@gsa.gov>
2023-06-20 13:18:46 -0700

    fix: persist font size with local storage

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -1,11 +1,16 @@
 //= require js/uswds.min.js
 
+function setFontSize(size) {
+  let htmlElement = window.document.getElementsByTagName('html')[0];
+  htmlElement.setAttribute('style', 'font-size:' + size + '%;')
+}
+
 function changeFontSize(id, size) {
   const button = window.document.getElementById(id)
 
   button.addEventListener("click", () => {
-    let htmlElement = window.document.getElementsByTagName('html')[0];
-    htmlElement.setAttribute('style', 'font-size:' + size + '%;')
+    window.localStorage.setItem("fontSize", size);
+    setFontSize(size)
   });
 }
 
@@ -13,5 +18,13 @@ function changeFontSize(id, size) {
   if (window.document) {
     changeFontSize('font-100-button', 100)
     changeFontSize('font-120-button', 120)
+  }
+
+  if (window.localStorage) {
+    let fontSize = window.localStorage.getItem("fontSize");
+
+    if (fontSize) {
+      setFontSize(fontSize)
+    }
   }
 })()
