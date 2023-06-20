1d4d3c517b5cab71ef0177e0b6edf3c37c0c2cf9
Andrew Burnes <andrew.burnes@gsa.gov>
2023-06-20 12:41:05 -0700

    fix: Update styling based on feedback

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -1,4 +1,17 @@
 //= require js/uswds.min.js
 
-// Add your custom javascript here
-console.log("Hi from Federalist");
+function changeFontSize(id, size) {
+  const button = window.document.getElementById(id)
+
+  button.addEventListener("click", () => {
+    let htmlElement = window.document.getElementsByTagName('html')[0];
+    htmlElement.setAttribute('style', 'font-size:' + size + '%;')
+  });
+}
+
+(function () {
+  if (window.document) {
+    changeFontSize('font-100-button', 100)
+    changeFontSize('font-120-button', 120)
+  }
+})()
