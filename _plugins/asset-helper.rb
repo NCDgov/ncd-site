3153272776ec6810b55684aebd12eb984848f196
Andrew Burnes <andrew.burnes@gsa.gov>
2024-04-24 08:59:08 -0700

    Upgrade to decap cms from netlify cms

--- _plugins/asset-helper.rb
+++ _plugins/asset-helper.rb
@@ -6,13 +6,13 @@ module AssetHelper
         FileUtils.cp_r('node_modules/uswds/dist/js/', 'assets/', verbose: true)
         FileUtils.cp_r('node_modules/uswds/dist/img/', 'assets/uswds/', verbose: true)
         FileUtils.cp_r('node_modules/uswds/dist/fonts/', 'assets/uswds/', verbose: true)
-        FileUtils.cp_r('node_modules/netlify-cms/dist/', 'assets/netlify-cms/', verbose: true)
+        FileUtils.cp_r('node_modules/decap-cms/dist/', 'assets/decap-cms/', verbose: true)
         FileUtils.cp_r('node_modules/video.js/dist/', 'assets/video-js/', verbose: true)
         FileUtils.cp_r('node_modules/videojs-youtube/dist/', 'assets/youtube-video-js/', verbose: true)
         @processed = true
     end
 end
-  
+
 Jekyll::Hooks.register :site, :after_reset do |site, payload|
     AssetHelper.process(site, payload)
 end
