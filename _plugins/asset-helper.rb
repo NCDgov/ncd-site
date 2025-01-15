82e37396df0657e5ee0e0b11c444f5b3850628df
Andrew Burnes <andrew.burnes@gsa.gov>
2025-01-15 13:18:59 -0700

    remove unused example data for videojs

--- _plugins/asset-helper.rb
+++ _plugins/asset-helper.rb
@@ -8,6 +8,8 @@ module AssetHelper
         FileUtils.cp_r('node_modules/uswds/dist/fonts/', 'assets/uswds/', verbose: true)
         FileUtils.cp_r('node_modules/decap-cms/dist/', 'assets/decap-cms/', verbose: true)
         FileUtils.cp_r('node_modules/video.js/dist/', 'assets/video-js/', verbose: true)
+        FileUtils.rm_rf('assets/video-js/examples')
+        FileUtils.rm_rf('assets/video-js/dist/examples')
         FileUtils.cp_r('node_modules/videojs-youtube/dist/', 'assets/youtube-video-js/', verbose: true)
         @processed = true
     end
